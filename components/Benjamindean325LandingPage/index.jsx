ea86523880d9a7da86df51b7b760e1dadb177598
Subtext Pro <hello@subtext.pro>
2022-12-28 22:24:29 -0500

    Initial commit

---
+++ components/Benjamindean325LandingPage/index.jsx
@@ -0,0 +1,225 @@
+import React from "react";
+import Group9 from "../Group9";
+import "./Benjamindean325LandingPage.css";
+
+function Benjamindean325LandingPage(props) {
+  const {
+    spanText1,
+    spanText2,
+    group1000006057,
+    frame,
+    spanText3,
+    spanText4,
+    spanText5,
+    spanText6,
+    spanText7,
+    spanText8,
+    spanText9,
+    spanText10,
+    group1,
+    spanText11,
+    spanText12,
+    line3,
+    pixelparrotclearbg11,
+    spanText13,
+    spanText14,
+    spanText15,
+    pixelparrotclearbg12,
+    spanText16,
+    spanText17,
+    spanText18,
+    spanText19,
+    spanText20,
+    spanText21,
+    spanText22,
+    be65307B6F17Cb232,
+    group2,
+    group33,
+    spanText23,
+    spanText24,
+    spanText25,
+    spanText26,
+    spanText27,
+    spanText28,
+    spanText29,
+    group27,
+    be65307B6F17Cb231,
+    group9Props,
+  } = props;
+
+  return (
+    <div className="container-center-horizontal">
+      <div className="benjamindean325-landing-page screen">
+        <div className="overlap-group5">
+          <div className="group-11">
+            <div className="group-8">
+              <div className="ellipse-container">
+                <div className="ellipse-141"></div>
+                <div className="ellipse-144"></div>
+              </div>
+              <div className="ellipse-142"></div>
+              <div className="ellipse-container-1">
+                <div className="ellipse-139"></div>
+                <div className="ellipse-143"></div>
+              </div>
+            </div>
+            <Group9 />
+            <Group9 className={group9Props.className} />
+          </div>
+          <div className="group-1000006054">
+            <h1 className="title spacegrotesk-bold-steel-gray-60px">
+              <span className="spacegrotesk-bold-steel-gray-60px">{spanText1}</span>
+            </h1>
+            <p className="beautiful-functional ibmplexmono-medium-scarpa-flow-26px">
+              <span className="ibmplexmono-medium-scarpa-flow-26px">{spanText2}</span>
+            </p>
+          </div>
+          <div className="group-1000006057" style={{ backgroundImage: `url(${group1000006057})` }}></div>
+          <div className="group-22">
+            <img className="frame" src={frame} alt="Frame" />
+            <div className="group-21">
+              <div className="about-pro-tech spacegrotesk-bold-steel-gray-48px">
+                <span className="spacegrotesk-bold-steel-gray-48px">{spanText3}</span>
+              </div>
+              <p className="we-create-amazing-co ibmplexmono-medium-scarpa-flow-18px">
+                <span className="ibmplexmono-medium-scarpa-flow-18px">{spanText4}</span>
+                <span className="ibmplexmono-medium-scarpa-flow-18px">{spanText5}</span>
+                <span className="ibmplexmono-medium-scarpa-flow-18px">{spanText6}</span>
+              </p>
+            </div>
+          </div>
+          <div className="frame-8">
+            <div className="group-container">
+              <div className="group-36">
+                <div className="beautiful-and-functional-colors spacegrotesk-bold-steel-gray-48px">
+                  <span className="span0">{spanText7}</span>
+                  <span className="spacegrotesk-bold-steel-gray-48px">{spanText8}</span>
+                </div>
+                <p className="the ibmplexmono-medium-scarpa-flow-18px">
+                  <span className="ibmplexmono-medium-scarpa-flow-18px">{spanText9}</span>
+                </p>
+                <p className="the ibmplexmono-medium-scarpa-flow-18px">
+                  <span className="ibmplexmono-medium-scarpa-flow-18px">{spanText10}</span>
+                </p>
+              </div>
+              <img className="group" src={group1} alt="Group" />
+            </div>
+            <div className="group-37">
+              <div className="color-filters spacegrotesk-bold-dodger-blue-28px">
+                <span className="spacegrotesk-bold-dodger-blue-28px">{spanText11}</span>
+              </div>
+              <p className="monokai-pro-comes-wi ibmplexmono-medium-scarpa-flow-18px">
+                <span className="ibmplexmono-medium-scarpa-flow-18px">{spanText12}</span>
+              </p>
+            </div>
+          </div>
+          <div className="overlap-group">
+            <img className="line-3" src={line3} alt="Line 3" />
+            <div className="ellipse-160"></div>
+            <img className="pixelparrotclear-bg-1" src={pixelparrotclearbg11} alt="pixelparrotclearBG 1" />
+          </div>
+          <div className="group-25">
+            <p className="a-color-scheme-for-focusing spacegrotesk-bold-steel-gray-48px">
+              <span className="spacegrotesk-bold-steel-gray-48px">{spanText13}</span>
+            </p>
+            <p className="monokai-pro-is-a-col ibmplexmono-medium-scarpa-flow-18px">
+              <span className="ibmplexmono-medium-scarpa-flow-18px">{spanText14}</span>
+            </p>
+            <div className="group-24">
+              <div className="ellipse-146"></div>
+              <div className="ellipse-147"></div>
+              <div className="ellipse-148"></div>
+              <div className="ellipse-149"></div>
+              <div className="ellipse-150"></div>
+              <div className="ellipse-151"></div>
+            </div>
+            <p className="monokai-pro-is-a-col-1 ibmplexmono-medium-scarpa-flow-18px">
+              <span className="ibmplexmono-medium-scarpa-flow-18px">{spanText15}</span>
+            </p>
+          </div>
+          <div className="group-1000006053">
+            <img className="pixelparrotclear-bg-1-1" src={pixelparrotclearbg12} alt="pixelparrotclearBG 1" />
+            <p className="x2022-your-company-all-rights-reserved valign-text-bottom ibmplexmono-normal-scarpa-flow-18px">
+              <span>
+                <span className="ibmplexmono-normal-scarpa-flow-18px">{spanText16}</span>
+              </span>
+            </p>
+          </div>
+          <div className="group-1000006058">
+            <div className="group-container-1">
+              <div className="group-container-2">
+                <div className="group-32">
+                  <div className="group-30">
+                    <p className="how-to-install-free ibmplexmono-bold-scooter-28px">
+                      <span className="ibmplexmono-bold-scooter-28px">{spanText17}</span>
+                    </p>
+                    <p className="x1-package-control ibmplexmono-medium-scarpa-flow-22px">
+                      <span className="ibmplexmono-medium-scarpa-flow-22px">{spanText18}</span>
+                    </p>
+                    <p className="you-can-freely-evalu ibmplexmono-semi-bold-scarpa-flow-22px">
+                      <span className="ibmplexmono-semi-bold-scarpa-flow-22px">{spanText19}</span>
+                      <span className="span1">{spanText20}</span>
+                      <span className="ibmplexmono-semi-bold-scarpa-flow-22px">{spanText21}</span>
+                    </p>
+                  </div>
+                  <div className="frame-7">
+                    <div className="buy-license ibmplexmono-semi-bold-white-18px">
+                      <span className="ibmplexmono-semi-bold-white-18px">{spanText22}</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="overlap-group1">
+                  <div className="frame-1">
+                    <div className="overlap-group-1">
+                      <img
+                        className="vector"
+                        src="https://anima-uploads.s3.amazonaws.com/projects/63ad04bf271484f4b798c6ce/releases/63ad04da0c5699f0e3a06976/img/vector-1@2x.png"
+                        alt="Vector"
+                      />
+                      <img className="be65307b6f17cb23-2" src={be65307B6F17Cb232} alt="be65307b6f17cb23 2" />
+                      <img className="group-1" src={group2} alt="Group" />
+                    </div>
+                  </div>
+                  <img className="group-33" src={group33} alt="Group 33" />
+                </div>
+              </div>
+              <div className="group-29">
+                <div className="overlap-group3">
+                  <div className="rectangle-4"></div>
+                  <div className="group-28">
+                    <div className="group-28-item ibmplexmono-semi-bold-star-dust-18px">
+                      <span className="ibmplexmono-semi-bold-star-dust-18px">{spanText23}</span>
+                    </div>
+                    <div className="pro ibmplexmono-semi-bold-white-18px">
+                      <span className="ibmplexmono-semi-bold-white-18px">{spanText24}</span>
+                    </div>
+                    <div className="group-28-item-1 ibmplexmono-semi-bold-scarpa-flow-18px">
+                      <span className="ibmplexmono-semi-bold-scarpa-flow-18px">{spanText25}</span>
+                    </div>
+                    <div className="ristretto ibmplexmono-semi-bold-scarpa-flow-18px">
+                      <span className="ibmplexmono-semi-bold-scarpa-flow-18px">{spanText26}</span>
+                    </div>
+                    <div className="group-28-item ibmplexmono-semi-bold-scarpa-flow-18px">
+                      <span className="ibmplexmono-semi-bold-scarpa-flow-18px">{spanText27}</span>
+                    </div>
+                    <div className="group-28-item-1 ibmplexmono-semi-bold-scarpa-flow-18px">
+                      <span className="ibmplexmono-semi-bold-scarpa-flow-18px">{spanText28}</span>
+                    </div>
+                    <div className="classic ibmplexmono-semi-bold-scarpa-flow-18px">
+                      <span className="ibmplexmono-semi-bold-scarpa-flow-18px">{spanText29}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="group-27" style={{ backgroundImage: `url(${group27})` }}>
+              <img className="be65307b6f17cb23-1" src={be65307B6F17Cb231} alt="be65307b6f17cb23 1" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Benjamindean325LandingPage;
---
+++ components/Group9/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Group9.css";
+
+function Group9(props) {
+  const { className } = props;
+
+  return (
+    <div className={`group-9 ${className || ""}`}>
+      <div className="ellipse-container-2">
+        <div className="ellipse-141-1"></div>
+        <div className="ellipse-144-1"></div>
+      </div>
+      <div className="ellipse-142-1"></div>
+      <div className="ellipse-container-3">
+        <div className="ellipse-139-1"></div>
+        <div className="ellipse-143-1"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Group9;
